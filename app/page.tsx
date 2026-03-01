be6bbda78658af5b5f19f30c61e004911f6652dd
Nico Pretorius <nicopretorius69@gmail.com>
2026-03-01 17:02:28 +1300

    Clean: Remove orphaned comment lines

--- app/page.tsx
+++ app/page.tsx
@@ -426,9 +426,7 @@ export default function Home() {
 
       
 
-            {/* Contact Section */}
 
-      {/* Contact Section */}
 
 
       <section id="contact" className="py-20 px-4 sm:px-6 lg:px-8 bg-gray-50">
